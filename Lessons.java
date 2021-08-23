697f40cbca3e8eda2feed14770270452a55b5a0e
jackPotTrash <barskaya.anna2111@gmail.com>
2021-08-23 20:19:57 +0500

    app print Hello World!

--- Lessons.java
+++ Lessons.java
@@ -1,7 +1,9 @@
 // This is my firs progrsmm
 
 class Lessons{
-	public static void main(String[] arc){
+	public static void main(String[] arc){//сперва модификатор тип название метода
+	
+	System.out.println("Hello World!")
 		
 		
 	}
