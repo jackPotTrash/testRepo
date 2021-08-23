517f955a766b4cbc6dfa61053e6f5f7ed5b01a04
jackPotTrash <barskaya.anna2111@gmail.com>
2021-08-23 20:42:16 +0500

    fixBug sum in string to println

--- Lesson01.java
+++ Lesson01.java
@@ -3,7 +3,7 @@ public static void main(String[] args){
 int a,b;
 a = 10;
 b = 1;
-System.out.println("sum of "+a+" and "+ b + " = "+a+b);
+System.out.println("sum of "+a+" and "+ b + " = "+(a+b));//если написать без скобок, получится 101
 }
 
 }
